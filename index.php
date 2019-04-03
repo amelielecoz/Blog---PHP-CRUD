9089bd6df1a8d0437abbdb3e35e4a1209f54a3a3
Le Coz <lecozamelie@hotmail.com>
2019-04-03 19:47:54 +0200

    split model from controller (index.php)

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
-try
-{
-	$bd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
 
-$req = $bd->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%i\') AS creation_date_fr FROM posts ORDER BY creation_date LIMIT 0, 5');
+require('modele.php');
 
+$req = getPosts();
 
+require('homeView.php');
+
+?>
