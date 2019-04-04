a582a55548bf4bc16a7885c3238179c898fbd0ff
Le Coz <lecozamelie@hotmail.com>
2019-04-04 15:57:16 +0200

    adapt files to new router config

--- homeView.php
+++ homeView.php
@@ -5,7 +5,7 @@
 <h2>Derniers billets du blog :</h2>
 
 <?php
-while ($data = $req->fetch())
+while ($data = $posts->fetch())
 {
     ?>
 
@@ -17,7 +17,7 @@ while ($data = $req->fetch())
                     <h5 class="card-title"><?=htmlspecialchars($data['title']) ?></h5>
                     <h6 class="card-subtitle mb-2 text-muted">Publié le <?= $data['creation_date_fr'] ?></h6>
                     <p class="card-text"><?= nl2br(htmlspecialchars($data['content'])) ?></p>
-                    <a href="post.php?id=<?= $data['id'] ?>"><button type="button" class="btn btn-outline-primary">Commentaires</button></a>
+                    <a href="index.php?action=post&id=<?= $data['id'] ?>"><button type="button" class="btn btn-outline-primary">Commentaires</button></a>
                 </div>
             </div>
        
@@ -28,7 +28,7 @@ while ($data = $req->fetch())
  
 <?php
 } 
-$req->closeCursor();
+$posts->closeCursor();
 ?>
 
 <?php $content = ob_get_clean(); ?>
