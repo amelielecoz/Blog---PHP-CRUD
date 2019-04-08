689ad1a3431f953cef01389b063f6c571517426d
Le Coz <lecozamelie@hotmail.com>
2019-04-08 15:02:41 +0200

    style homeView

--- view/frontend/homeView.php
+++ view/frontend/homeView.php
@@ -2,27 +2,28 @@
 
 <?php ob_start(); ?>
 
+
+
+
+<div class="container">
 <h2>Derniers billets du blog :</h2>
 
 <?php
 while ($data = $posts->fetch())
 {
     ?>
-
-<div class="container">
-    <div class="row d-flex">
-       
-            <div class="col-12 m-3 card " style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title"><?=htmlspecialchars($data['title']) ?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Publié le <?= $data['creation_date_fr'] ?></h6>
-                    <p class="card-text"><?= nl2br(htmlspecialchars($data['content'])) ?></p>
-                    <a href="index.php?action=post&id=<?= $data['id'] ?>"><button type="button" class="btn btn-outline-primary">Commentaires</button></a>
-                </div>
+    <div class="row ">
+        <div class="col-sm-12 m-3 card ">
+            <div class="card-body">
+                <h5 class="card-title"><?=htmlspecialchars($data['title']) ?></h5>
+                <h6 class="card-subtitle mb-2 text-muted">Publié le <?= $data['creation_date_fr'] ?></h6>
+                <p class="card-text"><?= nl2br(htmlspecialchars($data['content'])) ?></p>
+                <a href="index.php?action=post&id=<?= $data['id'] ?>"><button type="button" class="btn btn-outline-primary">Commentaires</button></a>
             </div>
+        </div>
        
     </div>
-</div>
+
 
 
  
@@ -30,6 +31,7 @@ while ($data = $posts->fetch())
 } 
 $posts->closeCursor();
 ?>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 
