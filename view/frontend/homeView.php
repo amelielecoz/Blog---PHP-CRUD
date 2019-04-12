1dbda8bf78c7c1a9a7d60c6e5042728a2dd9394c
Le Coz <lecozamelie@hotmail.com>
2019-04-12 10:33:31 +0200

    styling homeView (indentation)

--- view/frontend/homeView.php
+++ view/frontend/homeView.php
@@ -1,4 +1,8 @@
-<?php $title = 'Mon blog'; ?>
+<?php
+
+$title = 'Mon blog';
+
+?>
 
 <?php ob_start(); ?>
 
@@ -6,29 +10,28 @@
 
 
 <div class="container">
-<h2>Derniers billets du blog :</h2>
+    <h2>Derniers billets du blog :</h2>
 
-<?php
-while ($data = $posts->fetch())
-{
-    ?>
-    <div class="row ">
-        <div class="col-sm-12 m-3 card ">
-            <div class="card-body">
-                <h5 class="card-title"><?=htmlspecialchars($data['title']) ?></h5>
-                <h6 class="card-subtitle mb-2 text-muted">Publié le <?= $data['creation_date_fr'] ?></h6>
-                <p class="card-text"><?= nl2br(htmlspecialchars($data['content'])) ?></p>
-                <a href="index.php?action=post&id=<?= $data['id'] ?>"><button type="button" class="btn btn-outline-primary">Commentaires</button></a>
+    <?php
+    while ($data = $posts->fetch()) {
+            ?>
+        <div class="row ">
+            <div class="col-sm-12 m-3 card ">
+                <div class="card-body">
+                    <h5 class="card-title"><?= htmlspecialchars($data['title']) ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Publié le <?= $data['creation_date_fr'] ?></h6>
+                    <p class="card-text"><?= nl2br(htmlspecialchars($data['content'])) ?></p>
+                    <a href="index.php?action=post&id=<?= $data['id'] ?>"><button type="button" class="btn btn-outline-primary">Commentaires</button></a>
+                </div>
             </div>
+
         </div>
-       
-    </div>
 
 
 
- 
-<?php
-} 
+
+    <?php
+}
 $posts->closeCursor();
 ?>
 </div>
