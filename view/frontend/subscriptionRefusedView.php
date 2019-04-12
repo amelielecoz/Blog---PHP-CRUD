be0457de7b23a6b7cb150db8bc8e63a7f14f2bbd
Le Coz <lecozamelie@hotmail.com>
2019-04-12 10:22:40 +0200

    add Subscription response views

---
+++ view/frontend/subscriptionRefusedView.php
@@ -0,0 +1,14 @@
+<?php $title = 'Connexion'; ?>
+
+<?php ob_start(); ?>
+
+<div class="container">
+
+    <h1>Désolé, votre inscription n'a pas pu être enregistrée, veuillez réessayer!</h1>
+    <a href="index.php?action=subscription" class="btn btn-info m-2 my-sm-0">Revenir en arrière</a>
+
+</div>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/frontend/template.php'); ?>
