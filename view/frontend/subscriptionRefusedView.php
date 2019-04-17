8120818496b462bcbabb4d1f909d7f9c9849277e
Le Coz <lecozamelie@hotmail.com>
2019-04-17 17:11:26 +0200

    update view when subscription is refused

--- view/frontend/subscriptionRefusedView.php
+++ view/frontend/subscriptionRefusedView.php
@@ -1,9 +1,12 @@
-<?php $title = 'Connexion'; ?>
+<?php
+$title = 'Connexion';
+$error_subscription;
+?>
 
 <?php ob_start(); ?>
 
 <div class="container">
-
+    <div class="alert alert-danger"><?= $error_subscription ?></div>
     <h1>Désolé, votre inscription n'a pas pu être enregistrée, veuillez réessayer!</h1>
     <a href="index.php?action=subscription" class="btn btn-info m-2 my-sm-0">Revenir en arrière</a>
 
