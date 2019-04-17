677b5bf20135af2da23a3de9798a1155a81c8b49
Le Coz <lecozamelie@hotmail.com>
2019-04-17 17:00:01 +0200

    add TinyMCE plugin and update navbar

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -5,15 +5,54 @@
     <meta charset="utf-8" />
     <title><?= $title ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=9o0bwilvuxfu8apr3jfarzj1ne9onrrdyj4kdyet29nax94e"></script>
+    <script>
+        tinymce.init({
+            selector: '#myeditable-h5',
+            inline: true,
+            menubar: false,
+            branding: false,
+            toolbar: 'undo redo'
+        });
 
+        tinymce.init({
+            selector: '#myeditable-div',
+            branding: false,
+            menubar: false,
+            plugins: [
+                'link',
+                'textcolor',
+                'lists',
+                'powerpaste',
+                'autolink',
+                'tinymcespellchecker'
+            ]
+        });
+    </script>
 </head>
 
 <body>
+    <!--pre>
+     <?php
+
+        ?>
+    </pre-->
+
     <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand" href="index.php">Accueil</a>
         <form class="form-inline">
-            <a href="index.php?action=connexion" class="btn btn-info m-2 my-sm-0">Se connecter</a>
-            <a href="index.php?action=subscription" class="btn btn-info m-2 my-sm-0">S'inscrire</a>
+            <?php if (!isset($_SESSION['connected'])) : ?>
+                <a href="index.php?action=connexion" class="btn btn-info m-2 my-sm-0">Se connecter</a>
+                <a href="index.php?action=subscription" class="btn btn-info m-2 my-sm-0">S'inscrire</a>
+            <?php else :  ?>
+                <a href="#" style="text-decoration: none; font-size: 1em">
+                    <span style="font-size: 2em; color: Tomato; " class="m-2 pr-4">
+                        <?= ucwords($_SESSION['user']['userFirstName']) ?>
+                        <i class="fas fa-user-astronaut"> </i>
+                    </span>
+                </a> <a href="index.php?action=logout" class="btn btn-info m-2 my-sm-0">Se déconnecter</a>
+            <?php endif;  ?>
         </form>
     </nav>
     <div class="jumbotron jumbotron-fluid">
