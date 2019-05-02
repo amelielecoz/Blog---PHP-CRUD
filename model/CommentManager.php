f1b2bd70a9af085cd70317925dd9c4cac48cb5b0
Le Coz <lecozamelie@hotmail.com>
2019-05-02 14:25:07 +0200

    update model Classes

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -8,8 +8,14 @@ class CommentManager extends Manager
     public function getComments($postId)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
-        $comments->execute(array($postId));
+        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\')
+        AS comment_date_fr
+        FROM comments
+        WHERE post_id = ? AND status = 0
+        ORDER BY comment_date
+        DESC');
+        $comments->bindValue(1, $postId, PDO::PARAM_INT);
+        $comments->execute();
 
         return $comments;
     }
@@ -18,8 +24,31 @@ class CommentManager extends Manager
     {
         $db = $this->dbConnect();
         $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
-        $affectedLines = $comments->execute(array($postId, $author, $comment));
+        $comments->bindValue(1, $postId, PDO::PARAM_INT);
+        $comments->bindValue(2, $author, PDO::PARAM_STR);
+        $comments->bindValue(3, $comment, PDO::PARAM_STR);
+        $comments->execute();
 
-        return $affectedLines;
+        return $comments;
+    }
+
+    public function reportComment($postId)
+    {
+        $db = $this->dbConnect();
+        $reportedComment = $db->prepare('UPDATE comments SET status = 1 WHERE id = ?');
+        $reportedComment->bindValue(1, $postId, PDO::PARAM_INT);
+        $reportedComment->execute();
+
+        return $reportedComment;
+    }
+
+    public function deleteComments($postId)
+    {
+        $db = $this->dbConnect();
+        $deletedComment = $db->prepare('DELETE FROM comments WHERE post_id=?');
+        $deletedComment->bindValue(1, $postId, PDO::PARAM_INT);
+        $deletedComment->execute();
+
+        return $deletedComment;
     }
 }
