92cf703b73e48226d97835a89b0bf0909239ac0d
Le Coz <lecozamelie@hotmail.com>
2019-05-06 16:06:00 +0200

    reorganise router according to authorisations

--- index.php
+++ index.php
@@ -1,136 +1,164 @@
 <?php
-// // Import PHPMailer classes into the global namespace
-// // These must be at the top of your script, not inside a function
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
-
-// // Load Composer's autoloader
-// require '../../vendor/autoload.php';
-
-// // Instantiation and passing `true` enables exceptions
-// $mail = new PHPMailer(true);
-
-// try {
-//     //Server settings
-//     $mail->SMTPDebug = 2;                                       // Enable verbose debug output
-//     $mail->isSMTP();                                            // Set mailer to use SMTP
-//     $mail->Host       = 'smtp.office365.com';  // Specify main and backup SMTP servers
-//     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-//     $mail->Username   = 'blog@amelielecoz.com';                     // SMTP username
-//     $mail->Password   = '?s8MkxL%75';                               // SMTP password
-//     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-//     $mail->Port       = 26;                                    // TCP port to connect to
-
-//     //Recipients
-//     $mail->setFrom('from@example.com', 'Mailer');
-//     $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-//     $mail->addAddress('ellen@example.com');               // Name is optional
-//     $mail->addReplyTo('info@example.com', 'Information');
-//     $mail->addCC('cc@example.com');
-//     $mail->addBCC('bcc@example.com');
-
-//     // Attachments
-//     // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//     // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-//     // Content
-//     $mail->isHTML(true);                                  // Set email format to HTML
-//     $mail->Subject = 'Here is the subject';
-//     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-//     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-//     $mail->send();
-//     echo 'Message has been sent';
-// } catch (Exception $e) {
-//     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-// }
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 session_start();
 $error_subscription = null;
-require('controller/frontend.php');
-require('controller/backend.php');
-// isConnected();
-// forceUserConnected();
+require_once('controller/FrontendManager.php');
+require_once('controller/BackendManager.php');
 
 
 try {
-    if (isset($_SESSION['connected'])) {
-        if (isset($_GET['action'])) {
-            //Dashboard by default
-            if ($_GET['action'] == 'dashboard') {
-                dashboard();
-            }
+    $frontendManager = new FrontendManager();
+    $backendManager = new BackendManager();
 
-            //Dashboard write new article
-            elseif ($_GET['action'] == 'dashboardAddPost') {
-                dashboardAddPost();
-            }
-
-            //Dashboard write new article
-            elseif ($_GET['action'] == 'addPost') {
-                addPost();
-            }
-
-            //Dashboard with the list of posts
-            elseif ($_GET['action'] == 'dashboardListPosts') {
-                dashboardListPosts();
-            }
-
-            //Dashboard modify/delete view
-            elseif ($_GET['action'] == 'dashboardPost') {
-                dashboardOnePost();
-            }
-
-            //Logout page
-            elseif ($_GET['action'] == 'logout') {
-                logout();
-            }
-
-            //Allow user to add comments=
-            elseif ($_GET['action'] == 'addComment') {
-                comment();
-            }
-        } else {
-            dashboardListPosts();
-        }
-    } else {
+    if (!isset($_SESSION['connected'])) { // if user is not connected
 
         if (isset($_GET['action'])) {
-            //Home Page with posts listed
-            if ($_GET['action'] == 'listPosts') {
-                listPosts();
+
+            if ($_GET['action'] == 'contact') {
+                $frontendManager->contactForm();
             }
-
-            //Page with 1 post + comments
-            elseif ($_GET['action'] == 'post') {
-                showOnePost();
+            //Contact form answer
+            elseif ($_GET['action'] == 'contactAnswer') {
+                $frontendManager->contactFormAnswer();
             }
-
-
+            //About page
+            elseif ($_GET['action'] == 'about') {
+                $frontendManager->about();
+            }
+            //Privacy page
+            elseif ($_GET['action'] == 'privacy') {
+                $frontendManager->privacy();
+            }
 
             //Connexion page
             elseif ($_GET['action'] == 'connexion') {
-                showConnexionForm();
+                $frontendManager->connexionForm();
             }
 
             //connexion form answer
             elseif ($_GET['action'] == 'connexionAnswer') {
-                connexionFormAnswer();
+                $backendManager->connexionFormAnswer();
             }
-
-
 
             //Subscription page
             elseif ($_GET['action'] == 'subscription') {
-                showSubscriptionForm();
+                $frontendManager->subscriptionForm();
             }
 
             //Subscription form answer
             elseif ($_GET['action'] == 'subscribe') {
-                subscriptionFormAnswer();
+                $frontendManager->subscriptionFormAnswer();
             }
+
+            //Home Page with posts listed
+            elseif ($_GET['action'] == 'listPosts') {
+                $frontendManager->listPosts();
+            }
+
+            //Page with 1 post + comments
+            elseif ($_GET['action'] == 'post') {
+                $frontendManager->showOnePost();
+            } else { // if action does not correspond to any possibility listed
+                $frontendManager->listPosts();
+            }
+        } else { // if no action specified
+            $frontendManager->listPosts();
+        }
+    } elseif (isset($_SESSION['connected'])) { // If user is connected
+
+        if (isset($_GET['action'])) {
+
+            //Logout
+            if ($_GET['action'] == 'logout') {
+                $backendManager->logout();
+            }
+
+            //Dashboard with the list of posts
+            if ($_GET['action'] == 'dashboardListPosts' || $_GET['action'] == 'dashboard') {
+                $backendManager->dashboardListPosts();
+            }
+
+            //One post only
+            elseif ($_GET['action'] == 'post') {
+                $backendManager->dashboardOnePost();
+            }
+
+            //Redirect to form to write new article
+            elseif ($_GET['action'] == 'addPostForm') {
+                $backendManager->addPostForm();
+            }
+
+            //Confirm form and send article content in the db
+            elseif ($_GET['action'] == 'addPost') {
+                $backendManager->addPostFormAnswer();
+            }
+
+            //Confirm form and send article content in the db
+            elseif ($_GET['action'] == 'modifyPostForm') {
+                $backendManager->modifyPostForm();
+            }
+
+            //Confirm form and send article content in the db
+            elseif ($_GET['action'] == 'modifyPost') {
+                $backendManager->modifyPostFormAnswer();
+            }
+
+            //Allow user to add comments=
+            elseif ($_GET['action'] == 'addComment') {
+                $backendManager->comment();
+            }
+
+            //Report comment
+            elseif ($_GET['action'] == 'report') {
+                $backendManager->reportComment();
+            }
+
+            //Delete post
+            elseif ($_GET['action'] == 'delete') {
+                $backendManager->deletePost();
+            }
+
+            //Comments administration view
+            elseif ($_GET['action'] == 'commentAdmin') {
+                $backendManager->commentAdmin();
+            }
+
+            //Authorize comment
+            elseif ($_GET['action'] == 'authorizeComment') {
+                $backendManager->authorizeComment();
+            }
+
+            //Confirm comment deletion
+            elseif ($_GET['action'] == 'deleteComment') {
+                $backendManager->deleteComment();
+            }
+
+            //Contact form
+            if ($_GET['action'] == 'contact') {
+                $frontendManager->contactForm();
+            }
+
+            //Contact form answer
+            elseif ($_GET['action'] == 'contactAnswer') {
+                $frontendManager->contactFormAnswer();
+            }
+            //About page
+            elseif ($_GET['action'] == 'about') {
+                $frontendManager->about();
+            }
+
+            //Privacy page
+            elseif ($_GET['action'] == 'privacy') {
+                $frontendManager->privacy();
+            }
+
+            //For all the other entry of action
         } else {
-            listPosts();
+            $backendManager->dashboardListPosts();
         }
     }
 } catch (Exception $e) {
